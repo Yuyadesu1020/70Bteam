51d2e42c89f0b0199f2d1e760a97b37978c16384
yukke0803 <tiger5588@docomo.ne.jp>
2023-11-29 12:42:07 +0800

    DB,model,controllerの作成
    model.controllerの初期設定

--- app/Models/User.php
+++ app/Models/User.php
@@ -41,4 +41,14 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    function tasks()
+    {
+        return $this->hasMany('App\Models\Task');
+    }
+
+    function likes()
+    {
+        return $this->hasMany('App\Models\Like');
+    }
 }

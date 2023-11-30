4869e7554350fefbbbf5d77cc3ecb03cdcf9e715
Yuyadesu1020 <Puchiyongzai@gmail.com>
2023-11-30 19:06:50 +0800

    Update profile.blade.php

--- resources/views/tasks/profile.blade.php
+++ resources/views/tasks/profile.blade.php
@@ -21,10 +21,10 @@
           <div class="collapse navbar-collapse" id="navbarSupportedContent">
             <ul class="navbar-nav me-auto mb-2 mb-lg-0">
               <li class="nav-item">
-                <a class="nav-link active" aria-current="page" href="#">profile</a>
+                <a class="nav-link active" aria-current="page" href="#">post</a>
               </li>
               <li class="nav-item">
-                <a class="nav-link" href="#">post</a>
+                <a class="nav-link" href="#">edit</a>
               </li>
             </ul>
             <ul class="navbar-nav ms-auto">

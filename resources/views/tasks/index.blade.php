3b70b853cb48f94c6b8a76d09854a3fadd689c78
Yuyadesu1020 <Puchiyongzai@gmail.com>
2023-12-03 13:08:43 +0800

    indexとshowのルート編集

--- resources/views/tasks/index.blade.php
+++ resources/views/tasks/index.blade.php
@@ -23,7 +23,8 @@
           <div class="collapse navbar-collapse" id="navbarSupportedContent">
             <ul class="navbar-nav me-auto mb-2 mb-lg-0">
               <li class="nav-item">
-                <a class="nav-link active" aria-current="page" href="">profile</a>
+                <a class="nav-link active" aria-current="page" href="{{ route('user_posts', ['user'=> Auth::user()->id]) }}">profile</a>
+                {{-- user_postsにルートを繋げて、userの定義をする --}}
               </li>
               <li class="nav-item">
                 <a class="nav-link" href="{{ route('tasks.create') }}">post</a>

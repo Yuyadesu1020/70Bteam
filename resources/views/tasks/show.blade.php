b198d5289b9b8ed5b13257c5db5b6db2a5b36419
yukke0803 <tiger5588@docomo.ne.jp>
2023-12-07 14:23:43 +0800

    影ちゃん追加

--- resources/views/tasks/show.blade.php
+++ resources/views/tasks/show.blade.php
@@ -6,6 +6,7 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>moveon</title>
     <link rel="stylesheet" href="{{ asset('css/show.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/lightbox.css') }}">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">  
     <link href="https://use.fontawesome.com/releases/v6.5.0/css/all.css" rel="stylesheet">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
@@ -72,7 +73,7 @@
           {{-- {{ route('show',[$task->user->id]) }} --}}</div>
 
         <div class="card-out">
-          <img class="show-img" src="{{ asset($task->file_path) }}" alt="投稿の画像">
+          <a href="{{ asset($task->file_path) }}" data-lightbox="image-1" data-title="My caption" data-alt="Alt text"><img class="show-img" src="{{ asset($task->file_path) }}" alt="投稿の画像"></a>
           <div class="handwork">
           @if(Auth::check() && $task->user_id == Auth::user()->id) <!-- ✅ログイン者のみ編集・削除が可能 -->
             <div class="pencil-box">
@@ -167,5 +168,12 @@
         </div>
       </div>
   </div>
+  <script src="{{ asset('js/lightbox-plus-jquery.js') }}"></script>
+  <script>
+    lightbox.option({
+      'resizeDuration': 200,
+      'wrapAround': true
+    })
+</script>
 </body>
 </html>
